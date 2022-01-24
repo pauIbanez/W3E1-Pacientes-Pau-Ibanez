85277b9eadb72e663024758241a8feda5d747e0d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 15:13:07 +0100

    Added test for women average age

--- tests/patientsHandler.test.js
+++ tests/patientsHandler.test.js
@@ -48,4 +48,51 @@ describe("Given patientsHandler", () => {
       expect(result.nDiabeticMen).toBe(expectedDiabetisMan);
     });
   });
+  describe("When inputed 4 people of which 2 are 20yo women", () => {
+    test("Then it should return 20 as women average age", () => {
+      const inputPatients = [
+        {
+          patient: {
+            name: "Marta",
+            age: 20,
+            sexo: "W",
+          },
+          daysInHospital: 5,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Paca",
+            age: 20,
+            sexo: "W",
+          },
+          daysInHospital: 3,
+          diet: "Low fiber",
+        },
+      ];
+      const expectedWomenAverageAge = 20;
+
+      const result = patientsHandler(inputPatients);
+
+      expect(result.averageAgeWomen).toBe(expectedWomenAverageAge);
+    });
+  });
 });
