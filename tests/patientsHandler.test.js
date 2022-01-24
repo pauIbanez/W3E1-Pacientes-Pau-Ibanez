acb996cda418ed4ac00cde312dfd87a48490748c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 15:14:29 +0100

    Added test for number of adults

--- tests/patientsHandler.test.js
+++ tests/patientsHandler.test.js
@@ -95,4 +95,52 @@ describe("Given patientsHandler", () => {
       expect(result.averageAgeWomen).toBe(expectedWomenAverageAge);
     });
   });
+
+  describe("When inputed 4 people of which 3 are adults", () => {
+    test("Then it should return 3 as the number of adults", () => {
+      const inputPatients = [
+        {
+          patient: {
+            name: "Marta",
+            age: 20,
+            sexo: "W",
+          },
+          daysInHospital: 5,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 15,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Paca",
+            age: 20,
+            sexo: "W",
+          },
+          daysInHospital: 3,
+          diet: "Low fiber",
+        },
+      ];
+      const expectedNumberOfAdults = 3;
+
+      const result = patientsHandler(inputPatients);
+
+      expect(result.nAdults).toBe(expectedNumberOfAdults);
+    });
+  });
 });
