57cd79d9692b2bb32d9ba39520430e6ca965af80
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 15:09:22 +0100

    Added test for diabetic man counter

---
+++ tests/patientsHandler.test.js
@@ -0,0 +1,51 @@
+import patientsHandler from "../site/js/patentsHandler";
+
+describe("Given patientsHandler", () => {
+  describe("When inputed 4 people of which 2 are men with diabetis", () => {
+    test("Then it should return 2 diabetic men", () => {
+      const inputPatients = [
+        {
+          patient: {
+            name: "Marta",
+            age: 56,
+            sexo: "W",
+          },
+          daysInHospital: 5,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Esteban",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Diabetes",
+        },
+        {
+          patient: {
+            name: "Paco",
+            age: 40,
+            sexo: "M",
+          },
+          daysInHospital: 3,
+          diet: "Low fiber",
+        },
+      ];
+      const expectedDiabetisMan = 2;
+
+      const result = patientsHandler(inputPatients);
+
+      expect(result.nDiabeticMen).toBe(expectedDiabetisMan);
+    });
+  });
+});
