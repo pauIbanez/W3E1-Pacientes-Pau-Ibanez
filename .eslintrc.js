5b71dfca3307e9a3a4a7510bdfdbfb9669e0bef5
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 13:24:25 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/extensions": "off",
+  },
+};
